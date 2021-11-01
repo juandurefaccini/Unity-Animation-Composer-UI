20dde1b2752f4442d61607576c4f7247f2a6b7af
FacundoMozo <fmozo@alumnos.exa.unicen.edu.ar>
2021-11-01 01:30:13 -0300

    Script Boton Actualizado para tener en cuenta el caso de que se seleccione dos veces el mismo boton

--- UI-Animation-Composer/Assets/Scripts/AnimationComposerUI/Boton.cs
+++ UI-Animation-Composer/Assets/Scripts/AnimationComposerUI/Boton.cs
@@ -16,9 +16,18 @@ public class Boton : MonoBehaviour
 
     /// <summary>Cambia el color del boton y setea el boton anterior al color original - Autores : Camila Garcia Petiet
     /// </summary>
+    /// ACTUALIZACION 31/10/21 Facundo Mozo : Arreglado el metodo para tener en cuenta el caso en que se seleccione dos veces el mismo boton
     public void CambiarColor()
-    {
-        GetComponent<UnityEngine.UI.Image>().color = Color.green;
-        columna.GetComponent<Columna>().SetBlanco(transform.gameObject);
+    {   
+        //  Pregunto si el boton esta pintado o no
+        if ( GetComponent<UnityEngine.UI.Image>().color == Color.white)
+        {   //Si no esta pintado, vuelvo blanco el ultimo y lo pinto verde
+            columna.GetComponent<Columna>().SetBlanco(transform.gameObject);
+            GetComponent<UnityEngine.UI.Image>().color = Color.green;
+        }
+        else
+        {   //Si esta pintado, vuelvo blaco el ultimo, osea este 
+            columna.GetComponent<Columna>().SetBlanco(transform.gameObject);
+        }
     }
 }
