0768ef86c17334f16de5604a22ce433c45669745
Juan Dure <jdurefaccini@alumnos.exa.unicen.edu.ar>
2021-11-15 17:05:17 -0300

    agregar preview action composer

--- UI-Animation-Composer/Assets/AnimationDirector.cs
+++ UI-Animation-Composer/Assets/AnimationDirector.cs
@@ -1,17 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class AnimationDirector : MonoBehaviour
 {
     public GameObject ListaAcciones;
+    public GameObject TextoJson;
 
     public void GuardarAccionCompuesta()
     {
-        foreach (Transform child in ListaAcciones.transform) // por cada elemento del listado del listadoi
+        String text = "";
+        foreach (Accion child in ListaAcciones.transform.GetComponentsInChildren<Accion>()) // por cada elemento del listado del listadoi
         {
-            Debug.Log(child.name);
-            Debug.Log(child.gameObject.GetComponent<IGetJson>().GetJson());
+            text += "\n";
+            text += "\n";
+            text += "\n";
+            text += child.GetJson();
         }
+        Debug.Log(text);
+        TextoJson.GetComponent<TextMeshProUGUI>().text = text;
     }
+    
+    
 }
